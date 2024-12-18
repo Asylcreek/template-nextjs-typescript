c6694851df44a4ce06c61ec0360d5de40562e59a
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2024-12-18 11:39:02 +0100

    chore: Update to  Next 15. Upgrade to eslint 9. Update to Node 22.

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals",
+    "prettier",
+), {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    rules: {
+        "space-before-function-paren": "off",
+        "@typescript-eslint/space-before-function-paren": "off",
+
+        "@typescript-eslint/no-unused-vars": ["error", {
+            vars: "all",
+            args: "after-used",
+            ignoreRestSiblings: false,
+        }],
+
+        "react/no-unescaped-entities": "off",
+    },
+}];
